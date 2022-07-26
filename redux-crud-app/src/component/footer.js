d32b6d85df127529ac756ec092787d56a69d124c
LujainHSmadi <100359593+LujainHSmadi@users.noreply.github.com>
2022-07-26 11:42:43 +0300

    main

---
+++ redux-crud-app/src/component/footer.js
@@ -0,0 +1,21 @@
+
+
+const Footer = () => {
+    return (
+
+
+        <footer className="text-center text-lg-start bg-light text-muted">
+
+
+
+
+
+
+
+        </footer>
+
+
+    )
+}
+
+export default Footer;
---
+++ redux-crud-app/src/feature/users/userSlice.js
@@ -0,0 +1,51 @@
+import { createSlice } from "@reduxjs/toolkit";
+import Swal from "sweetalert2";
+
+
+
+const usersSlice = createSlice({
+    name: 'users',
+    initialState: {
+        email: "lujain@gmail.com",
+        password: "123456",
+        isLogged: false
+    },
+
+    reducers: {
+        login: (state, action) => {
+            console.log(action);
+            if (state.email === action.payload.email && state.password === action.payload.password) {
+
+                state.isLogged = true;
+                // Swal.fire({
+                //     position: 'top-end',
+                //     icon: 'success',
+                //     title: 'Wellcome',
+                //     showConfirmButton: false,
+                //     timer: 1500
+                //   })
+
+            } else {
+                // Swal.fire({
+                //     position: 'top-end',
+                //     icon: 'warning',
+                //     title: 'somthing went wrong',
+                //     showConfirmButton: false,
+                //     timer: 1500
+                //   })
+            }
+        },
+        logout: (state) => {
+            state.isLogged = false;
+            // Swal.fire({
+            //     position: 'top-end',
+            //     icon: 'success',
+            //     title: 'See you soon',
+            //     showConfirmButton: false,
+            //     timer: 1500
+            //   })
+        }
+    }
+});
+export const { login, logout } = usersSlice.actions;
+export default usersSlice.reducer;
